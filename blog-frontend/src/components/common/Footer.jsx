2c9df7d840488ae9303d0c4b655ec4d9a9b63293
sahilsapariya <sahilsapariya03@gmail.com>
2024-03-05 21:05:29 +0530

    footer done

---
+++ blog-frontend/src/components/common/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://sahilsapariya03.netlify.app" target="_blank">
+        © Sahil Sapariya, 2024
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
