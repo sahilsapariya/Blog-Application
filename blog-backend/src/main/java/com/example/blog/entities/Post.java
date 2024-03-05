119908553ce10ba5138cf150a02c8a085d505993
sahilsapariya <sahilsapariya03@gmail.com>
2024-03-05 18:06:22 +0530

    initial commit to the client side

--- blog-backend/src/main/java/com/example/blog/entities/Post.java
+++ blog-backend/src/main/java/com/example/blog/entities/Post.java
@@ -1,27 +1,15 @@
 package com.example.blog.entities;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collector;
 
 @Entity
-@Table
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,29 +18,14 @@ public class Post {
     @Lob
     private String content;
     private LocalDate createdOn;
-    private LocalDate updatedOn;
-    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinColumn(name = "blog_user_id")
-    private BlogUser blogUser;
-    @OneToMany(mappedBy = "post", cascade = {CascadeType.ALL}, fetch = FetchType.LAZY)
-    private List<Comment> comments;
 
     public Post() {
     }
 
-    public Post(String title, String content, LocalDate createdOn, LocalDate updatedOn, BlogUser blogUser, List<Comment> comments) {
+    public Post(String title, String content, LocalDate createdOn) {
         this.title = title;
         this.content = content;
         this.createdOn = createdOn;
-        this.updatedOn = updatedOn;
-        this.blogUser = blogUser;
-        this.comments = comments;
-    }
-
-    public Post updateContent(String updatedContent) {
-        setContent(updatedContent);
-        setUpdatedOn(LocalDate.now());
-        return this;
     }
 
     public Long getId() {
@@ -85,54 +58,5 @@ public class Post {
 
     public void setCreatedOn(LocalDate createdOn) {
         this.createdOn = createdOn;
-    }
-
-    public LocalDate getUpdatedOn() {
-        return updatedOn;
-    }
-
-    public void setUpdatedOn(LocalDate updatedOn) {
-        this.updatedOn = updatedOn;
-    }
-
-    public BlogUser getBlogUser() {
-        return blogUser;
-    }
-
-    public void setBlogUser(BlogUser blogUser) {
-        this.blogUser = blogUser;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public JsonNode asJson() {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.createObjectNode()
-                .put("id", id)
-                .put("title", title)
-                .put("content", content)
-                .put("createdOn", createdOn.toString())
-                .put("updatedOn", updatedOn.toString())
-                .put("blogUser", blogUser.getUserName())
-                .put("comments", comments.size());
-    }
-
-    public JsonNode asJsonWithComments() {
-        return ((ObjectNode) asJson())
-                .set("comments", getComments().stream()
-                        .map(Comment::asJson)
-                        .collect(Collector.of(() -> new ObjectMapper().createArrayNode(), ArrayNode::add, ArrayNode::addAll))
-                );
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" + "title='" + title + '\'' + ", content='" + content + '\'' + ", createdOn=" + createdOn + ", updatedOn=" + updatedOn + ", blogUser=" + blogUser.getUserName() + ", comments=" + comments.size() + '}';
     }
 }
--- blog-backend/src/main/java/com/example/blog/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.blog.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.blog.entities.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
