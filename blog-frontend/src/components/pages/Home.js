b3db20881220c9082216b18b062c5ecaec526c9b
sahilsapariya <sahilsapariya03@gmail.com>
2024-03-05 21:57:22 +0530

    home page post card design done

--- blog-frontend/src/components/pages/Home.js
+++ blog-frontend/src/components/pages/Home.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "../styles/Home.scss";
+import { posts } from "../../data/data";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home__container">
+      <BlogList blogs={posts} />
+    </div>
+  );
+};
 
-export default Home
+const BlogList = ({ blogs }) => {
+  return (
+    <div className="blog__list">
+      {blogs?.map((blog) => {
+        return <BlogCard key={blog.id} blog={blog} />;
+      })}
+    </div>
+  );
+};
+
+const BlogCard = ({ blog }) => {
+  return (
+    <div className="blog__card">
+      <div className="blog__header">
+        <h2>{blog.title}</h2>
+        <span>{blog.createdOn}</span>
+      </div>
+      <div className="blog__content">
+        <p>{blog.content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ blog-frontend/src/data/data.js
@@ -0,0 +1,26 @@
+export const posts = [
+    {
+        id: 1,
+        title: "Post 1",
+        content: "This is the content of post 1",
+        createdOn: "2021-08-01",
+    },
+    {
+        id: 2,
+        title: "Post 2",
+        content: "This is the content of post 2",
+        createdOn: "2021-08-01",
+    },
+    {
+        id: 3,
+        title: "Post 3",
+        content: "This is the content of post 3",
+        createdOn: "2021-08-01",
+    },
+    {
+        id: 4,
+        title: "Post 4",
+        content: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+        createdOn: "2021-08-01",
+    }
+]
